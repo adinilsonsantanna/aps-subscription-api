14dbe4e94f4b236b4db66b8a43e413a753303339
adinilsonsantanna <adinilsomsantanna@gmail.com>
2026-08-05 21:28:59 -0300

    TESTE

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,11 @@
 import { Router } from "express";
 
-import shopifyRoutes from "./shopify.routes";
-import subscriptionsRoutes from "./subscriptions.routes";
-import billingRoutes from "./billing.routes";
-import adminRoutes from "./admin.routes";
-import webhooksRoutes from "./webhooks.routes";
-
 const router = Router();
 
-router.use("/shopify", shopifyRoutes);
-router.use("/subscriptions", subscriptionsRoutes);
-router.use("/billing", billingRoutes);
-router.use("/admin", adminRoutes);
-router.use("/webhooks", webhooksRoutes);
+router.get("/", (_, res) => {
+    res.json({
+        status: "OK"
+    });
+});
 
 export default router;
