acd1b4adf7a15c38be8c845341e83ccaf6f84d04
adinilsonsantanna <adinilsomsantanna@gmail.com>
2026-08-05 22:21:44 -0300

    AJUSTE FINAL VERSEL.JSON

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../src/app';
 
-// Wrap Express app for Vercel serverless
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    return new Promise((resolve, reject) => {
-        app(req, res, (err: any) => {
-            if (err) return reject(err);
-            resolve(undefined);
-        });
+  return new Promise<void>((resolve, reject) => {
+    app(req, res, (err: any) => {
+      if (err) return reject(err);
+      resolve();
     });
+  });
 }
